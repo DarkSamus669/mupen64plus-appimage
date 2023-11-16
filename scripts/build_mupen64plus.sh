64db2dbd8fc0be7eb234fc5bd94eaac16bbe9d64
Shivansh Srivastava <95683373+DarkSamus669@users.noreply.github.com>
2023-11-16 13:56:11 +0530

    Update build_mupen64plus.sh

--- scripts/build_mupen64plus.sh
+++ scripts/build_mupen64plus.sh
@@ -2,15 +2,15 @@
 
 # Download mupen64plus source
 wget -q "https://github.com/mupen64plus/mupen64plus-core/releases/download/2.5/mupen64plus-bundle-src-2.5.tar.gz" \
-	 -O mupen64plus.tar.gz
+     -O mupen64plus.tar.gz
 
 # Extract mupen64plus source
 tar -xf mupen64plus.tar.gz
 
 # Build mupen64plus
 
-cd mupen64plus-bundle-src-2.5
+cd mupen64plus-bundle-src-2.5/source/mupen64plus-core/projects/unix
 
 make -j`nproc` all
 
-make install DESTDIR=AppDir/usr
+make install PREFIX=AppDir/usr
